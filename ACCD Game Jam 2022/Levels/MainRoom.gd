2b676f5fdaaf533c9533ba18baf0165c28ac5889
sammiemac <snv.macaraniag@gmail.com>
2022-03-13 08:46:21 -0700

    stuff

--- ACCD Game Jam 2022/Levels/MainRoom.gd
+++ ACCD Game Jam 2022/Levels/MainRoom.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if Global.levels_completed == 0:
+		$BodySuit.visible = false
+		$AirTank.visible = false
+		$Helmet.visible = false
+	
+	if Global.levels_completed == 1:
+		$BodySuit.visible = true
+		$AirTank.visible = false
+		$Helmet.visible = false
+		
+	if Global.levels_completed == 2:
+		$BodySuit.visible = true
+		$AirTank.visible = true
+		$Helmet.visible = false
+		
+	if Global.levels_completed == 3:
+		$BodySuit.visible = true
+		$AirTank.visible = true
+		$Helmet.visible = true
