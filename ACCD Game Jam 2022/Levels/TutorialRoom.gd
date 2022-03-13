3a58a7015657668158dc374d1365622da1b1059c
sammiemac <snv.macaraniag@gmail.com>
2022-03-13 01:36:39 -0800

    new tilemap, import items, tutorial

--- ACCD Game Jam 2022/Levels/TutorialRoom.gd
+++ ACCD Game Jam 2022/Levels/TutorialRoom.gd
@@ -1,20 +1,11 @@
 extends Node2D
 
-var lights = false
 
-func _process(_delta):
-	if lights:
-		if Input.is_action_just_pressed("ui_accept") and $LightLever.switch:
-			print("click")
-			$LightsOff.visible = false
-			$LightLever/AnimatedSprite.play("on")
-			$LightLever/Light2D.energy = 1
-#			$LightsOn.visible = true
-			$Player/Light2D.visible = false
 
-func _on_LightLever_lever_on():
-	print("gello")
-	lights = true
-#	if Input.is_action_just_pressed("ui_accept") and $LightLever.switch:
-#		print("click")
-#		$CanvasModulate.visible = false
+func _on_Tunnel_body_entered(body):
+	$Timer.start()
+
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Levels/MainRoom.tscn")
